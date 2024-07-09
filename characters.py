1a9fd3b3e2407eb41836e95603b9804e05aa227d
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2024-07-09 09:40:21 +0900

    Update characters.py

--- characters.py
+++ characters.py
@@ -3,28 +3,14 @@ with open('./melos.txt','r') as file:
     text = file.read()
 
 # %%
-# defaultdictを使わない場合
-char_count = dict()
-for c in text:
-    if c in char_count:
-        char_count[c] += 1
-    else:
-        char_count[c] = 1 
-
-# %%
-# defaultdictを使う場合
-from collections import defaultdict
-char_count = defaultdict(int)
-for c in text:
-    char_count[c] += 1
-
-# %%
 # Unicodeコードポイント順にソートして出力
-char_sorted_list = sorted(char_count.keys())
+char_sorted_list = sorted(set(text))
 print('Unicode/文字/utf-8/shift-jis/回数')
 for c in char_sorted_list:
     print(hex(ord(c)),
           repr(c),
           c.encode().hex(),
           c.encode('shift-jis').hex(),
-          char_count[c],sep='/')
+          text.count(c),sep='/')
+
+# %%
